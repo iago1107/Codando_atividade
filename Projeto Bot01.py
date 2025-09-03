61f5e22fae375198238e29feacb080fa203e5a94
iago1107 <156558884+iago1107@users.noreply.github.com>
2025-09-03 13:42:49 -0300

    Add files via upload

---
+++ Projeto Bot01.py
@@ -0,0 +1,9 @@
+name = input("Hello! What is your name?")
+age_input = input("How old are you?")
+age = int(age_input)
+bot_age = 3
+age_difference = age - bot_age
+color = input("What's your favorite color?")
+print(f"Nice to meet you, {name}!") 
+print(f"Você é {age_difference} anos mais velho que eu. Eu tenho apenas {bot_age} anos!")
+print(f"Oh, {color} é uma cor linda!")
