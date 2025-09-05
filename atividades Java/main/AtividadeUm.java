dbd327eb6a3796547db90bbc3e6484c62d33b7f7
iago1107 <iago1107@hotmail.com>
2025-09-04 22:52:18 -0300

    temperatura em fahrenhert

---
+++ atividades Java/main/AtividadeUm.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class AtividadeUm {
+
+    public static void main(String[] args) {
+
+        double celcius, fa; 
+
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("digite a Temperatura em Celcius:    ");
+        celcius = leitor.nextInt();
+        fa = (9 * celcius + 160) / 5;
+        System.err.println("A temperatura convertida em Fahrenheri e :"+ fa);
+
+        leitor.close();
+    
+
+
+
+
+        
+    }
+
+
+
+
+
+
+    }
+
